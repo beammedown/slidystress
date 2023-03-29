ba8785089e194c8870f3b486227126513278f1ac
beammedown <trongero007@gmail.com>
2023-03-29 21:53:02 +0200

    put a bit of load on a server

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::time::Instant;
+
+fn main() {
+    loop {
+        let start = Instant::now();
+        let _resp = reqwest::blocking::get("https://op.lsv-hessen.de");
+        let duration = start.elapsed();
+        println!("{:?}", duration);
+        match _resp {
+            Ok(_resp) => {
+                continue;
+            }
+            Err(err) => {
+                println!("Error: {}", err);
+                break;
+            }
+        }
+    }
+}
